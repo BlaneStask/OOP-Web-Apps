e966707e7b0c28f6ca068863d2e5a8747aa241e9
Blane Staskiewicz <bas6248@psu.edu>
2021-07-24 16:13:03 -0400

    Update got_trivia.java

--- got_trivia.java
+++ got_trivia.java
@@ -1,20 +1,28 @@
-//Description: Java application that is a trivia game for Game of Thrones, that uses a GUI.
-
 package qanda;
 import java.util.*;
 import javax.swing.JOptionPane;
 
+/**
+* <h1>Question and Answer</h1>
+* Java application that is a trivia game
+* for Game of Thrones, that uses swing to 
+* make a GUI.
+*
+* @author  Blane Staskiewicz
+*/
 public class got_trivia{
-	//maximum number of questions 
-	public static final int max_q = 10;
-	/** This method is the main/driver method
-	 * 
-	 * @call		calls other methods for values
-	 */
+	public static final int max_q = 10;  // maximum number of questions 
+	
+	/** 
+	* This method is the main/driver method
+	* 
+	* @param args  Unused
+	*/
 	public static void main(String[] args){
-		//opening prompt for trivia game
+		// opening prompt for trivia game
 		String prompt = "Welcome to the GoT Trivia Game! Enjoy!";
 		String title =  "GoT Trivia Game";
+		
 		javax.swing.JOptionPane.showMessageDialog(null, prompt, title, JOptionPane.INFORMATION_MESSAGE);
 		String[] questions = {"At the beginning of the series, how many children do Ned and Catelyn Stark have?",
 				"Who is the first character in the series to be called 'King in the North'?", 
@@ -24,19 +32,21 @@ public class got_trivia{
 				"Who is executed via molten gold?", "Who is Sansa Stark's first fiance?"};
 		String[] answers = {"5", "ROBB STARK", "A FUNERAL PYRE", "RICHARD MADDEN", "NYMERIA", "TYRELL", "GRAND MAESTER", 
 				"QUEEN OF THORNS", "VISERYS TARGARYEN", "JOFFREY BARATHEON"};
-	//random unused value for question
-        ArrayList<Integer> list = new ArrayList<Integer>();
-        for(int i = 0; i < max_q; i++){
-            list.add(new Integer(i));
-        }
-        Collections.shuffle(list);
+		
+		// random unused value for question
+		ArrayList<Integer> list = new ArrayList<Integer>();
 		for(int i = 0; i < max_q; i++){
-			//gets random value, asks questions and checks the answers
-	        //int num = rand_val(s);
+		    list.add(new Integer(i));
+		}
+		
+		Collections.shuffle(list);
+		for(int i = 0; i < max_q; i++){
+			// gets random value, asks questions and checks the answers
+	        	// int num = rand_val(s);
 			String res = new QuestionAndAnswer().setQuestion(questions[list.get(i)]);
 			new QuestionAndAnswer().setAnswer(answers[list.get(i)], res);
 			
-			//play again? but not again the last question
+			// play again? but not again the last question
 			if(i != 9){
 				String promptq = "Do you want to Play again?";
 				int again = javax.swing.JOptionPane.showConfirmDialog(null, promptq, title, JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE);
@@ -47,10 +57,8 @@ public class got_trivia{
 				}
 			}
 		}
-		//end message
+		// end message
 		String end = "Thanks for playing GoT Trivia Game! Goodbye!";
 		javax.swing.JOptionPane.showMessageDialog(null, end, title, JOptionPane.INFORMATION_MESSAGE);
 	}
 }
-
-
